f7dbec7c6ae586de2ee68021688f275f34b3ef21
NAVIch0k <ivan.safonov.09@mail.ru>
2023-01-23 12:21:35 +0300

    fix color svg

--- src/app/screen/Main/Main.jsx
+++ src/app/screen/Main/Main.jsx
@@ -1,18 +1,22 @@
-import React from 'react'
-import Image from 'next/image'
+import React, { useState } from 'react'
+import Shape from '../../../../public/svg/shape.svg'
+import Star from '../../../../public/svg/star.svg'
 //style
 import s from './Main.module.scss'
 
 const Main = () => {
+
+  cont[nav, setNav] = useState(true)
+
   return (
     <div className='flex justify-center gap-y-[70px]'>
       <div className='flex gap-x-[25px]'>
         <div className='flex gap-x-[10px]'>
-          <img src='/svg/shape.svg' className={s.navEl} width={18} height={16} />
+          <Shape color={'red'} className={s.navEl} />
           <p>Работы</p>
         </div>
         <div className='flex gap-x-[10px]'>
-          <Image src='/svg/star.svg' className={s.navEl} width={18} height={16} />
+          <Star color={'red'} className={s.star} />
           <p>Награды</p>
         </div>
       </div>
