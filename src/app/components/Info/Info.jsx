7dfe1b5c777fa7633b3112c6880eeacc3e9d4d06
ivan <ivan.safonov.09@mail.ru>
2023-01-24 10:25:01 +0300

    оптимизация info.jsx

--- src/app/components/Info/Info.jsx
+++ src/app/components/Info/Info.jsx
@@ -25,21 +25,34 @@ const Info = () => {
     }
   ]
 
+  const dataText = [
+    {
+      title: 'о сайте',
+      text: 'Lorem ipsum dolor sit amet consectetur. Eget maecenas at sit hendrerit nunc volutpat ac. Sit turpis cursus sem in lacinia in ipsum sagittis orci. Ultrices eu pellentesque non cursus. Viverra sed elit gravida luctus justo duis in massa. Pretium habitant donec fermentum nec eget ornare nisi sit. Enim dignissim a tempus tortor et vel porta adipiscing. Nisl adipiscing ut at amet lectus pellentesque. Habitant integer pellentesque tellus in pellentesque ut tortor. Pretium dictumst vivamus ut velit at. Nec tristique cursus vestibulum ultricies fames risus venenatis nullam. In sed elementum sed ultrices blandit mi adipiscing tortor pulvinar. Pellentesque et risus aliquam.',
+      id: 1
+    },
+    {
+      title: 'об икт',
+      text: 'Отделение ИКТ (Информационно-коммуникационные технологии) - это одно подразделений Колледжа Связи №54. Enim dignissim a tempus tortor et vel porta adipiscing. Nisl adipiscing ut at amet lectus pellentesque. Habitant integer pellentesque tellus in pellentesque ut tortor. Pretium dictumst vivamus ut velit at. Nec tristique cursus vestibulum ultricies fames risus venenatis nullam. In sed elementum sed ultrices blandit mi adipiscing tortor pulvinar.',
+      id: 2
+    },
+  ]
+
   return (
     <div className='flex flex-col gap-y-[70px] mb-[146px]'>
       <div className='flex gap-x-[30px]'>
-        <div className={s.infoCont}>
-          <h1>о сайте</h1>
-          <p>
-            Lorem ipsum dolor sit amet consectetur. Eget maecenas at sit hendrerit nunc volutpat ac. Sit turpis cursus sem in lacinia in ipsum sagittis orci. Ultrices eu pellentesque non cursus. Viverra sed elit gravida luctus justo duis in massa. Pretium habitant donec fermentum nec eget ornare nisi sit. Enim dignissim a tempus tortor et vel porta adipiscing. Nisl adipiscing ut at amet lectus pellentesque. Habitant integer pellentesque tellus in pellentesque ut tortor. Pretium dictumst vivamus ut velit at. Nec tristique cursus vestibulum ultricies fames risus venenatis nullam. In sed elementum sed ultrices blandit mi adipiscing tortor pulvinar. Pellentesque et risus aliquam.
-          </p>
-        </div>
-        <div className={s.infoCont}>
-          <h1>об икт</h1>
-          <p>
-            Отделение ИКТ (Информационно-коммуникационные технологии) - это одно подразделений Колледжа Связи №54. Enim dignissim a tempus tortor et vel porta adipiscing. Nisl adipiscing ut at amet lectus pellentesque. Habitant integer pellentesque tellus in pellentesque ut tortor. Pretium dictumst vivamus ut velit at. Nec tristique cursus vestibulum ultricies fames risus venenatis nullam. In sed elementum sed ultrices blandit mi adipiscing tortor pulvinar.
-          </p>
-        </div>
+        {
+          dataText.map(el => {
+            return (
+              <div className={s.infoCont} key={el.id}>
+                <h1>{el.title}</h1>
+                <p>
+                  {el.text}
+                </p>
+              </div>
+            )
+          })
+        }
       </div>
       <div className='flex-center gap-x-[43px]'>
         {
