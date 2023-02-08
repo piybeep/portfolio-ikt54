8edbc30d2018489f2fb63fe1b2f3b06dd1951e18
ivan <ivan.safonov.09@mail.ru>
2023-02-08 12:13:02 +0300

    add favicon

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,23 +1,34 @@
 import LayoutBasic from '@/app/Layout/LayoutBasic'
 import '@/styles/globals.css'
 import { Nunito } from '@next/font/google'
-import 'swiper/css';
-import 'swiper/css/pagination';
+import Head from 'next/head'
+import 'swiper/css'
+import 'swiper/css/pagination'
 
-const font = Nunito({ preload: true, subsets: ['cyrillic'], weight: ["400", "600", "700"], display: "swap" })
+const font = Nunito({
+  preload: true,
+  subsets: ['cyrillic'],
+  weight: ['400', '600', '700'],
+  display: 'swap'
+})
 
 export default function App({ Component, pageProps }) {
   const getLayout = Component.getLayout || ((page) => page)
   return (
-    <LayoutBasic>
-      {getLayout(<Component {...pageProps} />)}
-      <style jsx global>
-        {`
-        *{
-          font-family:${font.style.fontFamily},sans-serif;
-        }
-    `}
-      </style>
-    </LayoutBasic>
+    <>
+      <Head>
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+      <LayoutBasic>
+        {getLayout(<Component {...pageProps} />)}
+        <style jsx global>
+          {`
+            * {
+              font-family: ${font.style.fontFamily}, sans-serif;
+            }
+          `}
+        </style>
+      </LayoutBasic>
+    </>
   )
 }
